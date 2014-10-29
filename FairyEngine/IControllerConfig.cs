7cd5fe121c77fd247a465010724335d9685b865a
SteamB23 <steamb23@gmail.com>
2014-10-29 23:33:23 +0900

    컨트롤러 관련 클래스 및 인터페이스 추가

---
+++ FairyEngine/IControllerConfig.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+namespace SteamB23.FairyEngine
+{
+    interface IControllerConfig
+    {
+        float XAxisDeadZone
+        {
+            get;
+        }
+        float YAxisDeadZone
+        {
+            get;
+        }
+        Keys Up
+        {
+            get;
+        }
+        Keys Down
+        {
+            get;
+        }
+        Keys Right
+        {
+            get;
+        }
+        Keys Left
+        {
+            get;
+        }
+        Keys A
+        {
+            get;
+        }
+        Keys B
+        {
+            get;
+        }
+        Keys Start
+        {
+            get;
+        }
+    }
+}
